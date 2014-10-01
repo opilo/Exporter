0d58fab8c8d5f9decf43ea8f639b1ce2f5b18afc
Behzad Shabani <behzad.shabani@gmail.com>
2014-10-01 17:49:06 +0330

    Fix typo in config file

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,7 @@
 return [
     'chunk_size'        => 100,
     'store_path'        => storage_path() . '/export/',
-    'file_extension'    => '.ext',
+    'file_extension'    => '.csv',
     'relation_glue'     => '|',
     'csv_delimiter'     => ','
 ];
